5467ab969ca0e45cc01a728e11a2afd1ff78f67c
Em1lT <emilanton.toivainen@gmail.com>
2022-10-11 09:03:05 +0300

    Feat: tracing example

---
+++ ray/index.js
@@ -0,0 +1,29 @@
+let player;
+let walls = [];
+let numOfWalls = 10;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	player = new Player(random(width), random(height));
+	for (let i = 0; i < numOfWalls; i++) {
+		const wall = new Wall(
+			random(width),
+			random(width),
+			random(height),
+			random(height)
+		);
+		walls.push(wall);
+	}
+}
+
+function draw() {
+	background(220);
+	frameRate(0);
+	angleMode(DEGREES);
+	player.update(walls);
+	player.render();
+
+	for (let wall of walls) {
+		wall.render();
+	}
+}
