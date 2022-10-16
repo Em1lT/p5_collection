c71067e0ed2718a309fdaefdf81ad00c983e61ab
Em1lT <emilanton.toivainen@gmail.com>
2022-10-16 19:54:09 +0300

    Feat: bean smooth movement

--- bean/index.js
+++ bean/index.js
@@ -1,26 +1,29 @@
-let pos
-let degrVect
-let degr = 0
+let pos;
+let degrVect;
+let degr = 0;
 
 function setup() {
-  createCanvas(400, 400);
-  pos = createVector(200,200)
+    createCanvas(400, 400);
+    pos = createVector(200, 200);
 }
 
 function draw() {
-  background(220);
-  degrVect = createVector(sin(degr), cos(degr))
-  line(pos.x, pos.y, pos.x + degrVect.x * 5, pos.y + degrVect.y  * 5)
-}
+    background(220);
+    degrVect = createVector(sin(degr), cos(degr));
+    line(pos.x, pos.y, pos.x + degrVect.x * 20, pos.y + degrVect.y * 20);
+    if (keyIsDown(LEFT_ARROW)) {
+        degr += 0.1
+    }
 
-function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    degr++;
-  } else if (keyCode === RIGHT_ARROW) {
-    degr--;
-  } else if (keyCode === UP_ARROW) {
-    pos.add(degrVect.mult(10))
-  } else if (keyCode === DOWN_ARROW) {
-    pos.sub(degrVect.mult(10))
-  }
+    if (keyIsDown(RIGHT_ARROW)) {
+        degr -= 0.1
+    }
+
+    if (keyIsDown(UP_ARROW)) {
+        pos.add(degrVect.mult(0.5));
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+        pos.sub(degrVect.mult(0.5));
+    }
 }
