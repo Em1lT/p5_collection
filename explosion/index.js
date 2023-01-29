4aa67f0a222e7fec5fd481de39f04bddec5d3cd6
Em1lT <emilanton.toivainen@gmail.com>
2023-01-29 09:36:54 +0200

    Feat: explosion radius update

--- explosion/index.js
+++ explosion/index.js
@@ -1,21 +1,23 @@
 let particle = [];
 let ball;
+let size = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ball = createVector(width / 2, height / 2, 10, 10);
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < size; i++) {
+    const m = 40 / size;
     particle.push({
-      p: createVector(ball.x + sin(i) * 100, ball.y + cos(i) * 100),
+      p: createVector(ball.x + sin(m * i) * 100, ball.y + cos(i * m) * 100),
     });
   }
 }
 
 function draw() {
   ellipse(ball.x, ball.y, 30, 30);
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < size; i++) {
     ellipse(particle[i].p.x, particle[i].p.y, 30, 30);
-    particle[i].p.x += sin(i);
-    particle[i].p.y += cos(i);
+    /*particle[i].p.x += sin(i);
+    	  particle[i].p.y += cos(i); */
   }
 }
