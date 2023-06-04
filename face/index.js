f07412dedbcbb589f555ab8fc161414237e35260
Em1lT <emilanton.toivainen@gmail.com>
2023-06-04 07:10:05 +0300

    Feat: ml5 face recognition

--- face/index.js
+++ face/index.js
@@ -1,54 +1,59 @@
 // Initialize the Image Classifier method with MobileNet
-let i = 0;
-let label = "";
-let confidence = "";
 let videoId;
+let loaded = false;
 
-const classifier = ml5.imageClassifier(
-  "DoodleNet",
-  (modelLoaded = () => {
-    label = "Model Loaded!";
-  })
-);
-
-// Create a new facemesh method
-// const facemesh = ml5.facemesh(videoId, modelLoaded);
+let facemesh;
 
 // When the model is loaded
 function modelLoaded() {
   console.log("Model Loaded!");
+  loaded = true;
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO);
-  capture.size(windowWidth, windowHeight);
+  capture.size(200, 200);
   videoId = capture.id();
+  console.log(capture);
+  facemesh = ml5.facemesh(videoId, modelLoaded);
+}
+
+function correct(value) {
+  return value + 220;
+}
+
+function drawLines(topLeftPoint, bottomRightPoint) {
+  ellipse(correct(topLeftPoint[1]), correct(topLeftPoint[0]), 10, 10);
+  ellipse(correct(topLeftPoint[0]), correct(bottomRightPoint[1]), 10, 10);
+  ellipse(correct(bottomRightPoint[0]), correct(topLeftPoint[1]), 10, 10);
+  ellipse(correct(bottomRightPoint[0]), correct(bottomRightPoint[1]), 10, 10);
+  ellipse(correct(bottomRightPoint[0]), correct(bottomRightPoint[1]), 10, 10);
+
+  line(
+    correct(topLeftPoint[0]),
+    correct(topLeftPoint[1]),
+    correct(topLeftPoint[0]),
+    correct(bottomRightPoint[1])
+  );
+  line(
+    correct(topLeftPoint[0]),
+    correct(bottomRightPoint[1]),
+    correct(bottomRightPoint[0]),
+    correct(topLeftPoint[1])
+  );
 }
 
 function draw() {
-  frameRate(30);
-  textSize(50);
-
-  image(capture, 0, 0, 1080, 720);
-  text(label, 200, 100);
-  text(confidence, 200, 150);
-  if (i > 100) {
-    classifier.classify(capture, (err, res) => {
-      label = res[0].label;
-      confidence = res[0].confidence;
-    });
-
-    /* 
-    facemesh.predict(capture, (results) => {
-      // do something with the results
-      const topLeftPoint = results[0].boundingBox.topLeft;
-      const bottomRightPoint = results[0].boundingBox.bottomRight;
-      console.log(topLeftPoint[0]);
-      point(topLeftPoint[0][0], topLeftPoint[0][1]);
-      point(bottomRightPoint[0], bottomRightPoint[1]);
-    });*/
-    i = 0;
+  frameRate(3);
+  background(220);
+  if (!loaded && !videoId) {
+    return;
   }
-  i++;
+  image(capture, 0, 0, 1080, 720);
+  facemesh.predict(capture, (results) => {
+    const [topLeftPoint] = results[0].boundingBox.topLeft;
+    const [bottomRightPoint] = results[0].boundingBox.bottomRight;
+    drawLines(topLeftPoint, bottomRightPoint);
+  });
 }
