add52aa99571750cc1a12cf4d75ee002f9fa31a6
Em1lT <emilanton.toivainen@gmail.com>
2023-07-05 16:44:26 +0300

    Feat: cleaned & added comments to ray

--- ray/index.js
+++ ray/index.js
@@ -3,39 +3,39 @@ let walls = [];
 let numOfWalls = 10;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// player = new Player(random(width), random(height));
-	player = new Player(width / 2, height / 2);
-	for (let i = 0; i < numOfWalls; i++) {
-		const wall = new Wall(
-			random(width),
-			random(width),
-			random(height),
-			random(height)
-		);
-		walls.push(wall);
-	}
+  createCanvas(windowWidth, windowHeight);
+  // player = new Player(random(width), random(height));
+  player = new Ray(width / 2, height / 2);
+  for (let i = 0; i < numOfWalls; i++) {
+    const wall = new Wall(
+      random(width),
+      random(width),
+      random(height),
+      random(height)
+    );
+    walls.push(wall);
+  }
 }
 
 function draw() {
-	background(220);
-	frameRate(30);
-	angleMode(DEGREES);
-	player.update(walls);
-	player.render();
+  background(220);
+  frameRate(30);
+  angleMode(DEGREES);
+  player.update(walls);
+  player.render();
 
-	for (let wall of walls) {
-		wall.render();
-	}
+  for (let wall of walls) {
+    wall.render();
+  }
 }
 
 function keyPressed() {
-	if (keyCode === LEFT_ARROW) {
-		player.updateAngle(1);
-	} else if (keyCode === RIGHT_ARROW) {
-		player.updateAngle(-1);
-	} else if (keyCode === UP_ARROW) {
-		player.move()
-	} else if (keyCode === DOWN_ARROW) {
-	}
+  if (keyCode === LEFT_ARROW) {
+    player.updateAngle(1);
+  } else if (keyCode === RIGHT_ARROW) {
+    player.updateAngle(-1);
+  } else if (keyCode === UP_ARROW) {
+    player.move();
+  } else if (keyCode === DOWN_ARROW) {
+  }
 }
