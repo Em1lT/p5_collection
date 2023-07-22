0ec2140052c1f0d18ed9c5f5c2fd24057bc71ef7
Em1lT <emilanton.toivainen@gmail.com>
2023-07-22 14:12:49 +0300

    Feat: mass is relevant to size

--- gravity/ball.js
+++ gravity/ball.js
@@ -1,48 +1,48 @@
 class Ball {
   constructor(posX, posY, mass) {
-    this.pos = createVector(posX,posY)
-    this.vel = createVector(0,0)
-    this.acc = createVector(0,0)
-    this.r = 12
-    this.mass = mass
-    this.gravity = createVector(0,0.098)
+    this.pos = createVector(posX, posY);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.r = 12 * mass * 3;
+    this.mass = mass;
+    this.gravity = createVector(0, 0.098);
   }
-  
+
   force(f) {
-    let calcForce = createVector(0,this.mass)
-    calcForce.add(f)
-    this.acc.add(calcForce)
+    let calcForce = createVector(0, this.mass);
+    calcForce.add(f);
+    this.acc.add(calcForce);
   }
-  
+
   update() {
-  this.force(this.gravity)
-  this.edges()  
-  this.pos.add(this.vel)
-  this.vel.add(this.acc)
-  this.acc = createVector(0,0)
+    this.force(this.gravity);
+    this.edges();
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.acc = createVector(0, 0);
   }
-  
+
   render() {
-    ellipse(this.pos.x, this.pos.y, this.r * 2)
-    let velLine = p5.Vector.add(this.pos, this.vel)
-    line(this.pos.x, this.pos.y, velLine.x, velLine.y)
+    ellipse(this.pos.x, this.pos.y, this.r * 2);
+    let velLine = p5.Vector.add(this.pos, this.vel);
+    line(this.pos.x, this.pos.y, velLine.x, velLine.y);
   }
-  
-edges() {
-  if(this.pos.y >= height - this.r) {
-    this.pos.y = height - this.r + 4;
-    this.vel.y *= -0.8
-    // this.vel.mult(createVector(0,-0.7))
+
+  edges() {
+    if (this.pos.y >= height - this.r) {
+      this.pos.y = height - this.r + 4;
+      this.vel.y *= -0.8;
+      // this.vel.mult(createVector(0,-0.7))
+    }
+
+    if (this.pos.x >= width - this.r) {
+      this.pos.x = width - this.r;
+      this.vel.x *= -0.5;
+      // vel.mult(createVector(-1,0))
+    } else if (this.pos.x <= 0 + this.r) {
+      this.pos.x = 0 + this.r;
+      this.vel.x *= -0.5;
+      // vel.mult(createVector(1,0))
+    }
   }
-  
-  if(this.pos.x >= width - this.r) {
-    this.pos.x = width - this.r;
-    this.vel.x *= -0.5
-    // vel.mult(createVector(-1,0))
-  } else if(this.pos.x <= 0 + this.r) {
-    this.pos.x = 0 + this.r;
-    this.vel.x *= -0.5
-    // vel.mult(createVector(1,0))
-  }
-}
 }
--- gravity/index.js
+++ gravity/index.js
@@ -1,22 +1,25 @@
-let b1
+let b1, b2,b3;
 
 function setup() {
   createCanvas(400, 400);
-  b1 = new Ball(100, 100, 1)
-  b2 = new Ball(300, 100, 0.4)
+  b1 = new Ball(100, 100, 1);
+  b2 = new Ball(300, 100, 0.4);
+  b3 = new Ball(200, 100, 0.1);
 }
 
 function draw() {
   background(220);
-  fill('red')
-  b1.update()
-  b1.render()
-  
-  fill('green')
-  b2.update()
-  b2.render()
+  fill("red");
+  b1.update();
+  b1.render();
 
-  line(0,300,width, height)
+  fill("green");
+  b2.update();
+  b2.render();
+
+  fill("blue");
+  b3.update();
+  b3.render();
+
+  line(0, 300, width, height);
 }
-
-
