838b380f44333af720a670ebc79ef0e3c01684b4
Emil Toivainen <emilanton.toivainen@gmail.com>
2024-01-20 11:24:36 +0200

    Feat: add pixels

--- pixel/index.js
+++ pixel/index.js
@@ -1,16 +1,44 @@
-function setup() {}
+let mouse;
+let grid = []
+function setup() {
+  createCanvas(500, 500);
+  pixelDensity(1);
+  // create the grid with width and height of the canvas
+  for (let i = 0; i < width; i ++) {
+    grid[i] = [];
+    for (let j = 0; j < height; j++) {
+      grid[i][j] = {type: 'air', x: i, y: j};
+    }
+  }
+
+
+}
 
 function draw() {
-  let pink = color(255, 102, 204);
+  background(51);
+	frameRate(1);
   loadPixels();
-
-  let d = pixelDensity();
-  let halfImage = 4 * width * (height * d);
-  for (let i = 0; i < halfImage; i += 4) {
-    pixels[i] = red(pink);
-    pixels[i + 1] = green(pink);
-    pixels[i + 2] = blue(pink);
-    pixels[i + 3] = alpha(pink);
+  for (let i = 0; i < width; i ++) {
+   for (let j = 0; j < height; j++) {
+     const cell = grid[i][j];
+     var index = (cell.x + cell.y * width) * 4;
+      if (cell.type === 'sand') {
+         pixels[index] = 255;
+         pixels[index + 1] = 0;
+         pixels[index + 2] = 25;
+         pixels[index + 3] = 255;
+      }
+    }
   }
+
   updatePixels();
+}
+
+function mouseClicked() {
+	mouseClick = createVector(mouseX, mouseY);
+  createSand(mouseClick)
+}
+
+function createSand(mouseClick) {
+  grid[mouseClick.x][mouseClick.y] = {type: 'sand', x: mouseClick.x, y: mouseClick.y};
 }
