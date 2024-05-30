079bda0af8e04aed9f3eb80a2820e66c8e30a36a
Emil Toivainen <emilanton.toivainen@gmail.com>
2024-05-30 20:23:36 +0300

    feat: flock test

--- flock/dot.js
+++ flock/dot.js
@@ -6,25 +6,24 @@ class Dot {
     this.pathIndex = pathIndex;
   }
 
-  getClosest (dots) {
-    return dots.reduce((closest, item) => {
-      if(!closest) {
-        closest = item
-        return closest
+  getClosest(dots) {
+    const closest = dots.reduce((closest, item) => {
+      if (!closest) {
+        return item; // First iteration, set the first item as the closest
       }
-      const c1 = dist(item.location.x, item.location.y, this.location.x, this.location.y)
-      const c2 = dist(item.location.x, item.location.y, closest.location.x, closest.location.y)
-      if(c2 < c1) {
-        closest = item
-        return closest
+      const c1 = dist(item.location.x, item.location.y, this.location.x, this.location.y);
+      const c2 = dist(closest.location.x, closest.location.y, this.location.x, this.location.y);
+      if (c1 < c2) {
+        return item; // Current item is closer, so it becomes the new closest
       }
-      return closest
-    }, null)
-    
+      return closest; // No closer item found, keep the current closest
+    }, null);
+
+    return closest;
   }
 
   update (vector) {
-    this.location.add(vector);
+    this.location.sub(vector);
   }
 
   render () {
--- flock/index.js
+++ flock/index.js
@@ -4,16 +4,19 @@ dots = [];
 function setup() {
 	createCanvas(500, 500);  
   for(let i = 1; i <= numOfDots; i++) {
-    dots.push(new Dot(createVector(random(0, width - 10), random(0, height - 10)), i, undefined))
+    const location = createVector(random(0, width - 10), random(0, height - 10))
+    console.log(location)
+    dots.push(new Dot(location, i, undefined))
   }
 }
 
 function udpateDots () {
   dots.forEach((dot) => {
     const v = p5.Vector.random2D();
-    const closest = dot.getClosest(dots);
+    // const dotsArrCopy = [...dots]
+    // const closest = dot.getClosest(dotsArrCopy);
     dot.render()
-    dot.update(closest)
+    // dot.update(closest)
   })
 }
 
