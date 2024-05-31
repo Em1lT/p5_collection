ed3f59ccb102896ed596c795c5618188d648e8ae
Emil Toivainen <emilanton.toivainen@gmail.com>
2024-05-31 20:13:28 +0300

    feat: flock

--- flock/dot.js
+++ flock/dot.js
@@ -4,16 +4,21 @@ class Dot {
     this.location = location;
     this.index = index
     this.pathIndex = pathIndex;
+    this.speed = createVector(0,0)
+    this.vel = p5.Vector.random2D();
   }
 
   getClosest(dots) {
     const closest = dots.reduce((closest, item) => {
-      if (!closest) {
+      if (!closest && this.location.x !== item.location.x && this.location.y !== item.location.y) {
         return item; // First iteration, set the first item as the closest
+      }
+      if(!closest) {
+        return
       }
       const c1 = dist(item.location.x, item.location.y, this.location.x, this.location.y);
       const c2 = dist(closest.location.x, closest.location.y, this.location.x, this.location.y);
-      if (c1 < c2) {
+      if (c1 < c2 && c1 !== 0 && c2 !== 0) {
         return item; // Current item is closer, so it becomes the new closest
       }
       return closest; // No closer item found, keep the current closest
@@ -22,13 +27,61 @@ class Dot {
     return closest;
   }
 
+  pushAwayFromOthers(closest) {
+    if(!closest) {
+      return
+    }
+    this.vel = p5.Vector.add(closest.location, this.location);
+    this.vel.limit(0.1)
+    this.speed.add(this.vel)
+    this.speed.limit(1)
+    this.location.add(this.speed);
+		this.vel = createVector(0, 0);
+  }
+
+  steerToCenter() {
+    const s = createVector(width /2, height /2 )
+    this.vel = p5.Vector.sub(s, this.location);
+    this.vel.limit(0.1)
+    this.speed.add(this.vel)
+    this.speed.limit(1)
+    this.location.add(this.speed);
+		this.vel = createVector(0, 0);
+  }
+
   update (vector) {
-    this.location.sub(vector);
+    if(vector) {
+      // this.vel = p5.Vector.sub(vector.location, this.location);
+    }
+    const heading = p5.Vector.fromAngle(this.location.heading)
+    this.vel.add(heading)
+    this.drawArrow(this.location, this.location, 'green')
+    rotate(heading)
+    this.speed.add(this.vel)
+    this.speed.limit(3)
+    const hea = createVector(this.location.x, this.location.y)
+    line(this.location.x, this.location.y, hea.x, hea.y)
+    this.location.add(this.speed);
+		this.vel = createVector(0, 0);
   }
 
   render () {
-    ellipse(this.location.x, this.location.y, 10)
+    triangle(this.location.x + 10, this.location.y, this.location.x, this.location.y + 10, this.location.x + 30, this.location.y + 30);
 
+  }
+
+  drawArrow(base, vec, myColor) {
+    push();
+    stroke(myColor);
+    strokeWeight(3);
+    fill(myColor);
+    translate(base.x, base.y);
+    line(0, 0, vec.x, vec.y);
+    rotate(vec.heading());
+    let arrowSize = 7;
+    translate(vec.mag() - arrowSize, 0);
+    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+    pop();
   }
 
 }
--- flock/index.js
+++ flock/index.js
@@ -1,11 +1,10 @@
-numOfDots = 2
+numOfDots = 1
 dots = [];
 
 function setup() {
 	createCanvas(500, 500);  
   for(let i = 1; i <= numOfDots; i++) {
     const location = createVector(random(0, width - 10), random(0, height - 10))
-    console.log(location)
     dots.push(new Dot(location, i, undefined))
   }
 }
@@ -13,18 +12,19 @@ function setup() {
 function udpateDots () {
   dots.forEach((dot) => {
     const v = p5.Vector.random2D();
-    // const dotsArrCopy = [...dots]
-    // const closest = dot.getClosest(dotsArrCopy);
+    const dotsArrCopy = [...dots]
+    const closest = dot.getClosest(dotsArrCopy);
+    dot.update(closest)
+    // dot.steerToCenter()
+    // dot.pushAwayFromOthers(closest)
     dot.render()
-    // dot.update(closest)
   })
 }
 
 function draw() {
 	background(220);
-  frameRate(1);
+  // angleMode(DEGREES);
+  // frameRate(5);
   udpateDots()
 }
-
-
 
