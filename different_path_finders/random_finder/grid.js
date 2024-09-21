9cbf68d3bdfd1d94fa1308cfece51965026f6fba
Emil Toivainen <emilanton.toivainen@gmail.com>
2024-09-21 22:21:04 +0300

    feat: first alg

--- different_path_finders/random_finder/grid.js
+++ different_path_finders/random_finder/grid.js
@@ -3,6 +3,8 @@ class Grid {
     this.size = width / size;
     this.grid = [];
     this.boxSize = this.size;
+    this.path = [];
+    this.current = null;
   }
 
   setupSquares() {
@@ -14,6 +16,7 @@ class Grid {
       }
     }
     this.grid[0][0].mark = true;
+    this.current = this.grid[0][0];
     this.grid[this.size - 1][this.size - 1].finish = true;
   }
 
@@ -51,13 +54,77 @@ class Grid {
   }
 
   setupObstacles() {
-    for (let i = 0; i < gridSize; i++) {
-      for (let j = 0; j < gridSize; j++) {
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
         const s = Math.floor(Math.random() * 100);
         if (s > 80) {
-          grid[i][j].obstacle = true;
+          this.grid[i][j].obstacle = true;
         }
       }
     }
+  }
+
+  pathFind() {
+    while (!this.current.finish) {
+      const next = this.getNextRandomAdjacentElements(this.current.location.x, this.current.location.y);
+      console.log(next);
+      if (!next) {
+        return;
+      }
+      next.mark = true;
+      this.path.push(next);
+      this.current = next;
+    }
+  }
+
+  getNextRandomAdjacentElements(i, j) {
+    const adjacentElements = this.getAdjacentElements(i, j)
+    return adjacentElements[Math.floor(Math.random() * adjacentElements.length)];
+  }
+
+  getAdjacentElements(i, j) {
+    if(this.grid[i+1] !== undefined && this.grid[i+1][j] !== undefined && this.grid[i-1] !== undefined && this.grid[i-1][j] !== undefined) {
+       return [
+        this.grid[i][j+1],
+        this.grid[i][j-1],
+        this.grid[i+1][j],
+        this.grid[i-1][j],
+        // filter out if not air
+      ].filter(el => el !== undefined)
+    } else if (this.grid[i+1] === undefined) {
+       return [
+        this.grid[i][j+1],
+        this.grid[i][j-1],
+        this.grid[i-1][j],
+        // filter out if not air
+      ].filter(el => el !== undefined)
+    } else if (this.grid[i-1] === undefined) {
+       return [
+        this.grid[i][j+1],
+        this.grid[i][j-1],
+        this.grid[i+1][j],
+        // filter out if not air
+      ].filter(el => el !== undefined)
+    } else if (this.grid[i+1][j] === undefined && this.grid[i-1][j] === undefined) {
+      return [
+        this.grid[i][j+1],
+        this.grid[i][j-1],
+        // filter out if not air
+      ].filter(el => el !== undefined)
+    }
+  }
+
+  getNext(current) {
+    let next = null;
+    if (current.x > 0) {
+      next = this.grid[current.x - 1][current.y];
+    } else if (current.x < this.size - 1) {
+      next = this.grid[current.x + 1][current.y];
+    } else if (current.y > 0) {
+      next = this.grid[current.x][current.y - 1];
+    } else if (current.y < this.size - 1) {
+      next = this.grid[current.x][current.y + 1];
+    }
+    return next;
   }
 }
--- different_path_finders/random_finder/index.js
+++ different_path_finders/random_finder/index.js
@@ -5,10 +5,12 @@ function setup() {
   createCanvas(800, 800);
   grid = new Grid(gridSize);
   grid.setupSquares();
-  gird.setupObstacles();
+  grid.setupObstacles();
 }
 
 function draw() {
   background(220);
+  frameRate(2);
   grid.drawSquares();
+  grid.pathFind();
 }
